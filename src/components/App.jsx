93c1ac59d4605d89110d334f3b52ef00c44f954c
NadyaKonzevaya <borisova.nadya@gmail.com>
2022-07-05 20:00:24 +0300

    add Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,8 @@
+import user from "user.json";
+import Profile from "components/Profile";
+import Statistics from "./Statistics";
+import data from "data.json";
+
 export const App = () => {
   return (
     <div
@@ -10,7 +15,15 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={ data} />
     </div>
   );
 };
